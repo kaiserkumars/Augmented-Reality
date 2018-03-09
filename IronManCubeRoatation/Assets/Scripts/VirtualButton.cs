9e00050dc1dd827d6fe95bf102a3ec86c8acea13
kaiserkumars <96deepakkumar@gmail.com>
2018-03-09 12:09:05 +0530

    Basic Projects

---
+++ IronManCubeRoatation/Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,31 @@
+﻿using Vuforia;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
+	private GameObject VBButtonObject;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+		VBButtonObject = GameObject.Find ("RotateZombieButton");
+		VBButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		zombie = GameObject.Find ("zombie");
+	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb){
+		Debug.Log ("Presses");
+		zombie.transform.Rotate(new Vector3(0,Time.deltaTime*1000,0));
+	}
+
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb){
+		Debug.Log ("Releases");
+	}
+
+	// Update is called once per frame
+
+}
